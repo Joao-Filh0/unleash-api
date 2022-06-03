0acb71aeab50f827b4dbbcfb49461f2dd8877ea1
joao <joaoadmob2@gmail.com>
2022-06-03 17:08:23 -0300

    add host

--- main.py
+++ main.py
@@ -22,4 +22,6 @@ def post_features():
     return jsonify(r)
 
 
-app.run(debug=True)
+if __name__== '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True,host="0.0.0.0",port=port)
